b319e88a5ce878937de2db9ce028372038f29075
Arun Bakt <arunbakt@gmail.com>
2021-03-04 16:55:23 -0800

    chore: fix the config file

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
-exports connectionString = process.env.DATABASE_URL
-exports useFieldDataService = process.env.USE_FIELD_DATA
-exports fieldDataURL = process.env.FIELD_DATA_URL
+exports.connectionString = process.env.DATABASE_URL
+exports.useFieldDataService = process.env.USE_FIELD_DATA
+exports.fieldDataURL = process.env.FIELD_DATA_URL
