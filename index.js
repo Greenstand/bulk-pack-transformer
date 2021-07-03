7832b119ef65544e763dabead4dd170d1ef56c69
Carlos Martins <luacmartins@gmail.com>
2021-07-03 09:20:51 -0700

    fix: fix conflicts

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const pool = new Pool({
 
 pool.on('connect', (client) => {
   console.log(`connected ${client}`);
-})
+});
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
@@ -37,50 +37,66 @@ app.use((err, req, res, next) => {
 app.set('view engine', 'html');
 
 app.post('/planter', async (req, res) => {
-  const planter = await data.findOrCreateUser(req.body.planter_identifier, req.body.first_name, req.body.last_name, req.body.organization);
+  const planter = await data.findOrCreateUser(
+    req.body.planter_identifier,
+    req.body.first_name,
+    req.body.last_name,
+    req.body.organization,
+  );
   const body = { ...req.body };
   body.phone = planter.phone;
   body.email = planter.email;
-  await data.createPlanterRegistration(planter.id, req.body.device_identifier, body);
+  await data.createPlanterRegistration(
+    planter.id,
+    req.body.device_identifier,
+    body,
+  );
   console.log(`processed planter ${planter.id}`);
   res.status(200).json({});
 });
 
 app.post('/tree', async (req, res) => {
-    const user = await data.findUser(req.body.planter_identifier);
-    if(user == null){
-      res.status(404).json({'error' : `planter not found ${req.body.planter_identifier}`})
-      return
-    }
+  const user = await data.findUser(req.body.planter_identifier);
+  if (user == null) {
+    res
+      .status(404)
+      .json({ error: `planter not found ${req.body.planter_identifier}` });
+    return;
+  }
 
-    let duplicate = null;
-    if(req.body.uuid !== null 
-      && req.body.uuid !== undefined
-      && req.body.uuid !== ""){
-      duplicate  = await data.checkForExistingTree(req.body.uuid);
-    }
-    if(duplicate !== null){
-      res.status(200).json({ duplicate });
-    } else if(config.useFieldDataService === "true") {
-        // translate to field-data capture payload
-        const tree = req.body
-        const capture = { 
-          ...tree,
-          planter_id: user.id
-        };
-        const options = {
-          method: 'POST',
-          uri: `${config.fieldDataURL}raw-captures`,
-          body: capture,
-          json: true // Automatically stringifies the body to JSON
-        };
-        const fieldCapture = await rp(options);
-        res.status(201).json({ fieldCapture });
-    } else {
-        const tree = await data.createTree( user.id, req.body.device_identifier, req.body);
-        console.log(`created tree ${tree.uuid}`);
-        res.status(201).json({ tree });
-    }
+  let duplicate = null;
+  if (
+    req.body.uuid !== null &&
+    req.body.uuid !== undefined &&
+    req.body.uuid !== ''
+  ) {
+    duplicate = await data.checkForExistingTree(req.body.uuid);
+  }
+  if (duplicate !== null) {
+    res.status(200).json({ duplicate });
+  } else if (config.useFieldDataService === 'true') {
+    // translate to field-data capture payload
+    const tree = req.body;
+    const capture = {
+      ...tree,
+      planter_id: user.id,
+    };
+    const options = {
+      method: 'POST',
+      uri: `${config.fieldDataURL}raw-captures`,
+      body: capture,
+      json: true, // Automatically stringifies the body to JSON
+    };
+    const fieldCapture = await rp(options);
+    res.status(201).json({ fieldCapture });
+  } else {
+    const tree = await data.createTree(
+      user.id,
+      req.body.device_identifier,
+      req.body,
+    );
+    console.log(`created tree ${tree.uuid}`);
+    res.status(201).json({ tree });
   }
 });
 
@@ -95,8 +111,8 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
 
 module.exports = app;
